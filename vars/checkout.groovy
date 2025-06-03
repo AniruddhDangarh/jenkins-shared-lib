1b91da565adf23368a6761cd6711acb8d5365d11
AniruddhDangarh <aniruddh.dangarh.snaatak@mygurukulam.co>
2025-06-03 12:11:18 +0530

    Create checkout.groovy

---
+++ vars/checkout.groovy
@@ -0,0 +1,10 @@
+def call(String branch, String repoUrl ) {
+    script {
+        echo "Checking out branch: ${branch} from repository: ${repoUrl}"
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: branch]],
+            userRemoteConfigs: [[url: repoUrl]]
+        ])
+    }
+}
