cbc800c36bee9accd440546ba5e64dab85decda9
AniruddhDangarh <aniruddh.dangarh.snaatak@mygurukulam.co>
2025-06-03 12:17:17 +0530

    Update QualityGate.groovy

--- vars/QualityGate.groovy
+++ vars/QualityGate.groovy
@@ -1,7 +1,8 @@
-def call()
+def call(){
  script {
-      def qg = waitForQualityGate()
-      if (qg.status != 'OK') {
-        error "Pipeline aborted due to quality gate failure: ${qg.status}"
+   def qg = waitForQualityGate()
+   if (qg.status != 'OK') {
+    error "Pipeline aborted due to quality gate failure: ${qg.status}"
+    }
   }
 }
